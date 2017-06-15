bb1d4d4d186ba6d254803327798fe9788f35659b
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-15 14:30:38 +0530

    Function to open archive

--- src/unrardll/wrapper.cpp
+++ src/unrardll/wrapper.cpp
@@ -16,6 +16,36 @@
 #define STRFY2(x) STRFY(x)
 #define NOMEM PyErr_SetString(PyExc_MemoryError, "Out of memory at line number: " STRFY2(__LINE__))
 
+static PyObject *UNRARError = NULL;
+
+static inline void
+convert_rar_error(unsigned int code) {
+#define CASE(x) case x: PyErr_SetString(UNRARError, "Error from unrar dll: " #x); break;
+    switch(code) {
+        CASE(ERAR_SUCCESS)             
+        CASE(ERAR_END_ARCHIVE)        
+        CASE(ERAR_NO_MEMORY)          
+        CASE(ERAR_BAD_DATA)           
+        CASE(ERAR_BAD_ARCHIVE)        
+        CASE(ERAR_UNKNOWN_FORMAT)     
+        CASE(ERAR_EOPEN)              
+        CASE(ERAR_ECREATE)            
+        CASE(ERAR_ECLOSE)             
+        CASE(ERAR_EREAD)              
+        CASE(ERAR_EWRITE)             
+        CASE(ERAR_SMALL_BUF)          
+        CASE(ERAR_UNKNOWN)            
+        CASE(ERAR_MISSING_PASSWORD)   
+        CASE(ERAR_EREFERENCE)         
+        CASE(ERAR_BAD_PASSWORD)       
+
+        default:
+            PyErr_SetString(UNRARError, "Unknown error");
+            break;
+    }
+#undef CASE
+}
+
 static wchar_t *
 unicode_to_wchar(PyObject *o) {
     wchar_t *buf;
@@ -43,6 +73,47 @@ wchar_to_unicode(const wchar_t *o) {
     return ans;
 }
 
+static const char* NAME = "RARFileHandle";
+
+static void 
+close_encapsulated_file(PyObject *capsule) {
+    HANDLE file = (HANDLE)PyCapsule_GetPointer(capsule, NAME);
+    if (file != NULL) RARCloseArchive(file);
+}
+
+
+static inline PyObject*
+encapsulate(HANDLE file) {
+    PyObject *ans = NULL;
+    if (!file) return NULL;
+    ans = PyCapsule_New(file, NAME, close_encapsulated_file);
+    if (ans == NULL) { RARCloseArchive(file); return NULL; }
+    return ans;
+}
+
+
+static PyObject*
+open_archive(PyObject *self, PyObject *args) {
+    PyObject *path = NULL, *extract = NULL;
+    RAROpenArchiveDataEx open_info = {0};
+    RARHeaderDataEx file_info = {0};
+    HANDLE rar_file = 0;
+
+    if (!PyArg_ParseTuple(args, "O!O", PyUnicode_Type, &path, &extract)) return NULL;
+    open_info.OpenMode = PyObject_IsTrue(extract) ? RAR_OM_EXTRACT : RAR_OM_LIST;
+    open_info.ArcNameW = unicode_to_wchar(path);
+    if (open_info.ArcNameW == NULL)  goto end;
+
+    rar_file = RAROpenArchiveEx(&open_info);
+    if (!rar_file) {
+        convert_rar_error(open_info.OpenResult);
+        goto end;
+    }
+
+end:
+    free(open_info.ArcNameW);
+    return encapsulate(rar_file);
+}
 
 // Boilerplate {{{
 struct module_state {
@@ -57,6 +128,11 @@ static struct module_state _state;
 #endif
 
 static PyMethodDef methods[] = {
+    {"open_archive", (PyCFunction)open_archive, METH_VARARGS,
+        "open_archive(path, extract=False)\n\nOpen the RAR archive at path. By default opens for listing, use extract=True to open for extraction."
+    },
+
+
     {NULL, NULL}
 };
 
@@ -113,6 +189,7 @@ initunrar(void)
         Py_DECREF(module);
         INITERROR;
     }
+    UNRARError = st->error;
 
 #if PY_MAJOR_VERSION >= 3
     return module;
