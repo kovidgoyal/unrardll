339d065247dd3b2a17f71e4bb34a34b7ac5682d2
Kovid Goyal <kovid@kovidgoyal.net>
2023-03-09 13:58:01 +0530

    version 0.1.6

--- src/unrardll/__init__.py
+++ src/unrardll/__init__.py
@@ -15,8 +15,8 @@ from . import unrar
 
 V = namedtuple('Version', 'major minor patch')
 
-str_version = '0.1.5'
-version = V(*map(int, str_version.split('.')))
+version = V(0, 1, 6)
+str_version = '{}.{}.{}'.format(*version)
 RARDLL_VERSION = unrar.RARDllVersion
 iswindows = hasattr(sys, 'getwindowsversion')
 isosx = 'darwin' in sys.platform.lower()
